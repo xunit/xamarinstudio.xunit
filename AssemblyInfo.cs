ec3c0b2d512fd4b8a4d66c6384251395c0f5804a
Sergey Khabibullin <sergey@khabibullin.com>
2015-06-14 14:56:05 +0300

    [XUnit] Revert to the previous version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyProduct ("MonoDevelop")]
 [assembly: AssemblyTitle ("xUnit.NET support")]
 [assembly: AssemblyDescription ("Integrates xUnit.NET into the MonoDevelop IDE.")]
-[assembly: AssemblyVersion ("0.4")]
+[assembly: AssemblyVersion ("0.5")]
 [assembly: AssemblyCopyright ("MIT X11")]
