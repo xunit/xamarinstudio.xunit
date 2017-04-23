ba3dd69ea50af92fac5bff8a569ab8840bbe4ed5
Lex Li <support@lextm.com>
2017-04-23 15:11:50 -0400

    Revised the script to include compilation.

--- dist.package.local.sh
+++ dist.package.local.sh
@@ -1,1 +1,3 @@
+msbuild MonoDevelop.UnitTesting.XUnit.sln /t:clean
+msbuild MonoDevelop.UnitTesting.XUnit.sln
 mono /Applications/Visual\ Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe setup pack MonoDevelop.XUnit/bin/Debug/MonoDevelop.XUnit.dll
