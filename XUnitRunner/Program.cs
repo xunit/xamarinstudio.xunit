33ca93367d3814a3e58fe52a567dc83ef3dc5466
Lex Li <support@lextm.com>
2017-09-28 19:25:32 -0400

    Added Rollbar to collect crashes.

--- XUnitRunner/Program.cs
+++ XUnitRunner/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
 using MonoDevelop.Core.Execution;
+using RollbarDotNet;
 
 namespace XUnitRunner
 {
@@ -15,9 +18,32 @@ namespace XUnitRunner
 				return;
 			}
 
+			RegisterRollbar();
+
 			XUnitRunner.PreloadAssemblies();
 			RemoteProcessServer server = new RemoteProcessServer();
 			server.Connect(args, new RemoteXUnitRunner(server));
+		}
+
+		private static void RegisterRollbar()
+		{
+			Rollbar.Init(new RollbarConfig {
+				AccessToken = "ab0cc28be33c444fbf877dd320c89598",
+				Environment = "production"
+			});
+			var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+			var userName = $"{version}";
+			Rollbar.PersonData(() => new Person(version) {
+				UserName = userName
+			});
+
+			AppDomain.CurrentDomain.UnhandledException += (sender, args) => {
+				Rollbar.Report(args.ExceptionObject as System.Exception);
+			};
+
+			TaskScheduler.UnobservedTaskException += (sender, args) => {
+				Rollbar.Report(args.Exception);
+			};
 		}
 	}
 }
