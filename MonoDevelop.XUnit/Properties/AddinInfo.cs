a7c3c91ed0d661152d974f597673815dde3f0255
Lex Li <support@lextm.com>
2017-09-27 23:26:51 -0400

    Bumped the version.

--- MonoDevelop.XUnit/Properties/AddinInfo.cs
+++ MonoDevelop.XUnit/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"MonoDevelop.XUnit2", 
 	Namespace = "MonoDevelop.XUnit",
-	Version = "0.7.10"
+	Version = "0.7.11"
 )]
 
 [assembly:AddinName ("xUnit.NET 2 testing framework support")]
